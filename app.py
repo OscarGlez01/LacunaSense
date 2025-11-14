fac0bf1c7f18eb8a7a22390b802de358fabf7ba1
Oscar Gonzalez Leyva <60475217+OscarGlez01@users.noreply.github.com>
2025-11-14 06:41:07 -0700

    idk

--- app.py
+++ app.py
@@ -158,13 +158,13 @@ st.sidebar.image(str(LOGO_PATH), width=120)
 st.sidebar.markdown("<h2>LacunaSense</h2>", unsafe_allow_html=True)
 st.sidebar.markdown("---")
 
-if st.sidebar.button("Inicio",type="tertiary"):
+if st.sidebar.button("Inicio",type="secondary"):
     st.session_state.screen = "home"
-if st.sidebar.button("Análisis",type="tertiary"):
+if st.sidebar.button("Análisis",type="secondary"):
     st.session_state.screen = "upload"
-if st.sidebar.button("Historial",type="tertiary"):
+if st.sidebar.button("Historial",type="secondary"):
     st.session_state.screen = "history"
-if st.sidebar.button("Sobre de",type="tertiary"):
+if st.sidebar.button("Sobre de",type="secondary"):
     st.session_state.screen = "about"
 
 st.sidebar.markdown("---")
